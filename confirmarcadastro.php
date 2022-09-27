5829e0519f8d20a48b74ebf93782a2eccb7ea93c
Gabriel Augusto <86443677+bielsantanape@users.noreply.github.com>
2022-09-27 11:43:42 -0700

    Update confirmarcadastro.php

--- confirmarcadastro.php
+++ confirmarcadastro.php
@@ -2,6 +2,11 @@
 
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-    echo "<meta http-equiv='refresh' content='0;URL=../solicitarservico.html'>";
+    echo "<meta http-equiv='refresh' 
+  if($email == "fulano@teste.com.br"){
+    if(senha =="senha123"){
+        content='0;URL=../solicitarservico.html'>";
+    }
+  }
 
 ?>
